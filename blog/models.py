a557d985c9e9b7ac59ed248db3061b84ee058ed8
YHyunCho <yhwmb321@gmail.com>
2022-06-26 16:08:58 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model) :
   created_at = models.DateTimeField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
 
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
   # 포스트 제목이 뜨게 해주는 코드
   def __str__(self) :
